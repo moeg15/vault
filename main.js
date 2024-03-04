d9476fc62b278bb7555b6ea381aa81180ec3dca8
moses <mosesgreenstein137@gmail.com>
2024-03-04 14:44:43 -0500

    done

---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+first define the msg varible
+then define the three equations varibles
+then 
+
+
+*/
+//defines the messesge
+const msg ="You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//equals 10
+const num1 = 4 + 6;
+//equals 40
+const num2 = 45-5;
+//equals 39
+const num3= 3*13;
+//sends alert msg
+alert(`${msg}${num1},${num2},${num3}`);
